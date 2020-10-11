383e4021605c3af02cf18b6309213ba9071b8140
michaelshmichael <michaeldcjohnson@gmail.com>
2020-10-11 20:37:44 +0600

    created repo and skeleton files

---
+++ script.js
@@ -0,0 +1,14 @@
+// object with the board functionality
+const gameBoard = ( () => {
+    let board = ["","","","","","","","","",];
+})();
+
+// player factory function
+const Player = (name, sign) => {
+    return{name, sign}
+};
+
+//game
+const game = ( () => {
+
+})();
