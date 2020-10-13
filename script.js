de8929670ca6b6f4165afba4b1458daaf76fae1a
michaelshmichael <michaeldcjohnson@gmail.com>
2020-10-13 17:16:30 +0600

    given some factory object pattern

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
-// object with the board functionality
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Object with the board functionality
 const gameBoard = ( () => {
-    let board = ["x","","o","o","x","o","o","o","",];
-    const boardSquares = document.querySelectorAll('.boardSquare');
-    
-    let counter = 0;
-    
-    boardSquares.forEach((square) => {
-        square.textContent = board[counter];
-        counter ++;
-    })
+    let board = ["1","2","o","?","x","o","@","o","t",];
+    const boardSquares = Array.from(document.querySelectorAll('.boardSquare'));
+    console.log(boardSquares);
 
+    function render(){
+        for(i = 0; i < board.length; i++){
+            boardSquares[i].textContent = board[i];
+        }
+    };
+
+    return {render};
 })();
 
-// player factory function
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Player Factory Function
 const Player = (name, sign) => {
-    return{name, sign}
+    const active = false;
+    return{name, sign, active}
 };
 
-//game
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Object with Game Logic
 const game = ( () => {
 
+    const player1 = Player("Player 1", "X");
+    const player2 = Player("Player 2", "O");
+
+    function changePlayer() {
+        player1.active = (player1.active) ? false : true;
+        player2.active = (player2.active) ? false : true;
+        console.log(2+2)
+    }
+    
+    function getActivePlayerSign() {
+        return (player1.active) ? player1.sign : player2.sign;
+    }
+
+    function startGame(){
+        gameBoard.render()
+    };
+    return{changePlayer, getActivePlayerSign, startGame};
 })();
 
+game.startGame();
