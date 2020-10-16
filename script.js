07e2256bb105c8139b019e3dded6b980257d81ee
michaelshmichael <michaeldcjohnson@gmail.com>
2020-10-16 08:55:10 +0600

    made checking game win functionarlity, but very dry code and there must be a better way with array manipulation

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ const Player = (name, sign) => {
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Object with the board functionality
 const gameBoard = ( () => {
+
     let board = ["","","","","","","","","",];
+    let winningCombinations = [
+        [0,3,6],[1,4,7],[2,5,8],[0,1,2],[3,4,5],[6,7,8],
+        [0,4,8],[2,4,6]
+    ];
+
     const boardSquares = Array.from(document.querySelectorAll('.boardSquare'));
+
 
     function addEventListeners(){
         boardSquares.forEach((square) =>{
@@ -23,6 +30,7 @@ const gameBoard = ( () => {
         game.swapPlayer();
         e.target.removeEventListener("click", updateBoard);
         console.log(board)
+        checkForWin();
     }
 
     function render(){
@@ -30,10 +38,31 @@ const gameBoard = ( () => {
             boardSquares[i].textContent = board[i];
         }
     };
+
+    //Really WET Solution - Surely a better way with array manipulation
+    function checkForWin(){
+        if((board[0] === board[3]) && (board[0] === board[6]) && board[0] != ""){
+            console.log(`Player ${board[0]} WON`)
+        } else if ((board[1] === board[4]) && (board[1] === board[7]) && board[1] != ""){
+            console.log(`Player ${board[1]} WON`)
+        } else if ((board[2] === board[5]) && (board[2] === board[8]) && board[2] != ""){
+            console.log(`Player ${board[2]} WON`)
+        } else if ((board[0] === board[1]) && (board[0] === board[2]) && board[0] != ""){
+            console.log(`Player ${board[0]} WON`)
+        } else if ((board[3] === board[4]) && (board[3] === board[5]) && board[3] != ""){
+            console.log(`Player ${board[3]} WON`)
+        } else if ((board[6] === board[7]) && (board[6] === board[8]) && board[6] != ""){
+            console.log(`Player ${board[6]} WON`)
+        } else if ((board[0] === board[4]) && (board[0] === board[8]) && board[0] != ""){
+            console.log(`Player ${board[0]} WON`)
+        } else if ((board[2] === board[4]) && (board[2] === board[6]) && board[2] != ""){
+            console.log(`Player ${board[2]} WON`)
+        }
+    }
     return {render, addEventListeners, updateBoard};
 })();
 
-// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Object with Game Logic
 const game = ( () => {
     const player1 = Player("Player 1", "X");
